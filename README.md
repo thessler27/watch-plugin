10bc2845a3439f659133a73ad3757b6c761c041e
Thomas Hessler <thomas@burnerfitness.com>
2017-08-10 14:16:53 -0400

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+---
+title: Background Audio
+description: Connect to Apple Watch via Watch Connectivity framework and send/receive messages.
+---
+
+## Installation
+
+1. `cordova plugin add https://github.com/thessler27/watch-plugin.git
+
+## Utilization
+
+1. Initialize: `window.plugins.applewatch.initializeSession():Promise<boolean>`. Returns `true` if successful connection, and throws an error (`false`) if unsuccessful.
+
+
+Supported Platforms
+-------------------
+
+- iOS
---
+++ src/ios/AppleWatch.m
@@ -0,0 +1,50 @@
+#import "AppleWatch.h"
+#import "WatchConnectivity/WatchConnectivity.h"
+
+@implementation AppleWatch
+
+// this method is executed when the app loads because of the onload param in plugin.xml
+- (void)pluginInitialize {
+    NSLog(@"Initializing Apple Watch plugin!");
+}
+
+- (void) initializeSession: (CDVInvokedUrlCommand *) command {
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult = nil;
+        if ([WCSession isSupported]) {
+            WCSession* session = [WCSession defaultSession];
+            session.delegate = self;
+            [session activateSession];
+            pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsBool: true];
+        }
+        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsBool: false];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId: command.callbackId];
+    }];
+}
+
+- (void) sendMessage: (CDVInvokedUrlCommand *) command {
+    [self.commandDelegate runInBackground:^{
+
+       NSString *teststring = [NSString stringWithFormat:@"%s", "Hello World"];
+       NSDictionary *applicationData = [[NSDictionary alloc] initWithObjects:@[teststring] forKeys:@[@"testdata"]];
+       CDVPluginResult* pluginResult = nil;
+      
+      [[WCSession defaultSession] sendMessage:applicationData
+                                  replyHandler:^(NSDictionary<NSString *,id> * _Nonnull replyMessage) {
+                                      //handle success
+                                      NSLog(@"Successful connection to default session! %@", replyMessage);
+                                      pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsBool: true];
+                                      [self.commandDelegate sendPluginResult:pluginResult callbackId: command.callbackId];
+                                  } errorHandler:^(NSError * _Nonnull error) {
+                                      //handle error
+                                      NSLog(@"Houston, we have a problem. %@", error);
+                                      pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsBool: false];
+                                      [self.commandDelegate sendPluginResult:pluginResult callbackId: command.callbackId];
+                                  }];
+    }];
+}
+
+
+
+
+@end
