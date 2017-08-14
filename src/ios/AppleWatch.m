6719f955c3a14ccc36e25fc6ca21237544e864f6
Thomas Hessler <thomas@burnerfitness.com>
2017-08-14 09:19:21 -0400

    more

--- src/ios/AppleWatch.m
+++ src/ios/AppleWatch.m
@@ -25,8 +25,11 @@
   // In this case, the message content being sent from the app is a simple begin message. This tells the app to wake up and begin sending location information to the watch.
   NSLog(@"Reached IOS APP %@", message);
   NSError *error; 
-  NSString *ogString = @"";
-  NSLog(@"JS Method to run %@", [ogString stringByAppendingFormat:@"setTimeout(function(){ console.log('cordova avail?', !!cordova); if(!!cordova) { cordova.fireDocumentEvent('cordovaAppleWatch:didReceiveMessage', {data: %@} ) } })", message]);
+  NSData  *jsonData = [NSJSONSerialization dataWithJSONObject:message 
+                                                      options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                        error:&error];
+  NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+  NSLog(@"JS Method to run %@", [NSString stringByAppendingFormat:@"setTimeout(function(){ console.log('cordova avail?', !!cordova); if(!!cordova) { cordova.fireDocumentEvent('cordovaAppleWatch:didReceiveMessage', {data: %@} ) } })", jsonString]);
   [self.commandDelegate evalJs:@"setTimeout(function(){ console.log('cordova avail?', !!cordova); if(!!cordova) { cordova.fireDocumentEvent('cordovaAppleWatch:didReceiveMessage', {data: 'Hello World'} ) } })"];
   
 }
